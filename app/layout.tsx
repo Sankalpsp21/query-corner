3ce9006333fb9aef0b7bacb142bf5a8a0f794c8a
Sankalp Patil <bothellsandy@gmail.com>
2024-03-15 01:41:24 -0700

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "@/app/globals.css";
 import { ThemeProvider } from "@/components/providers/ThemeProvider"
 import Nav from "@/components/Nav";
@@ -46,6 +48,8 @@ export default function RootLayout({
             </CustomBackground>
           </ThemeProvider>
         </ConvexClientProvider>
+        <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   )
